79488ea760805631d29d5566457983c6b9dc7d65
MAZEAPI.COM <55346746+mazeapi@users.noreply.github.com>
2019-11-25 22:20:16 +0600

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+echo Website are you?
+
+read website
+
+echo What is your database name?
+
+read database
+
 sudo add-apt-repository ppa:ondrej/php;
 sudo apt install php7.3 php7.3-soap php7.3-zip php7.3-curl php7.3-xml php7.3-gd php7.3-intl php7.3-bcmath php7.3-mysql mysql-server php7.3-fpm nginx php7.3-mbstring git vim zip htop -y;
 
@@ -19,7 +27,7 @@ sudo service php7.3-fpm restart;
 
 composer global config http-basic.repo.magento.com f92d6b866405d0799d86b41ffe00e342 378bc0e72c91dcaa404266bdf87ee961;
 
-cd /var/www/website.com;
+cd /var/www/$website;
 composer create-project --repository-url=https://repo.magento.com/ magento/project-community-edition=2.3.3 .;
 
 bin/magento setup:install --base-url=http://tutorial.magento2.dev \
